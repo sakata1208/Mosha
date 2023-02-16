31653abd3108306c28941d32370e7c6369aad3be
Sakata1208 <sakata.a1208@gmail.com>
2023-02-17 08:28:26 +0900

    アコーディオンメニューのボタン ホバー時の色

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(function () {
   $('.ac_parent').hover(function () {
   $(this).children(".ac_child").stop().slideToggle();
+  //openクラスを付ける
+  $(this).toggleClass("open")
+  $(".ac_parent").not(this).removeClass("open")
 });
 });
 
