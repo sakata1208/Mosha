0c263a832af0d1de87fac18584b60320ab2ef823
Sakata1208 <sakata.a1208@gmail.com>
2023-02-17 16:24:07 +0900

    質問投稿用にコメントなど追記

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
+//アコーディオンメニュー
 $(function () {
   $('.ac_parent').hover(function () {
-  $(this).children(".ac_child").stop().slideToggle();
-  //openクラスを付ける
+  //ホバーしたらメニューが開く
+    $(this).children(".ac_child").stop().slideToggle();
+  
+  //+ボタンの操作
   $(this).toggleClass("open")
   $(".ac_parent").not(this).removeClass("open")
 });
 });
 
+//ハンバーガーメニュー
 $(function(){
   $(".hamburger_btn").on("click",function(){
     $(".hamburger_btn,.header_nav").toggleClass("active")
